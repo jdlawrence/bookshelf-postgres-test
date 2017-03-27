4d6937c6e2033e536fd4353c265a351dc561df0a
jdlawrence <jamil.lawrence@gmail.com>
2017-03-26 20:31:32 -0700

    init

---
+++ knexfile.js
@@ -0,0 +1,64 @@
+// Update with your config settings.
+
+module.exports = {
+  test: {
+    client: 'postgresql',
+    connection: {
+      user: 'postgres',
+      database: 'dummy',
+      port: 5432,
+      host: 'localhost',
+      password: ''
+    },
+    debug: false,
+    pool: {
+      min: 1,
+      max: 2
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }, 
+    seeds: {
+      director: './seeds/test'
+    }
+  },
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      user: 'postgres',
+      database: 'dummy',
+      port: 5432,
+      host: 'localhost',
+      password: ''
+    },
+    debug: false,
+    pool: {
+      min: 1,
+      max: 2
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      user: 'postgres',
+      database: 'dummy',
+      port: 5432,
+      host: 'localhost',
+      password: ''
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
+
+
